50f273936e5652aba01e4ab5bd2082dcbc896265
Sam Orme <ormesam@hotmail.com>
2020-06-19 15:06:49 +0100

    Implement more complex way of generating a map

--- MapGenerator/Generator.cs
+++ MapGenerator/Generator.cs
@@ -4,11 +4,20 @@ using System.Text;
 namespace MapGenerator {
     public class Generator {
         private readonly int size;
+        private readonly int difficulty;
         private readonly Random random;
+        private readonly int threshold;
+        private readonly int edgeBias;
+        private readonly int minFreedom;
 
-        public Generator(int size) {
+        public Generator(int size, int difficulty) {
             this.size = size;
+            this.difficulty = difficulty;
             random = new Random();
+
+            threshold = GetWeight();
+            edgeBias = GetEdgeBias();
+            minFreedom = GetMinFreedom();
         }
 
         public void Run(int numberToCreate) {
@@ -24,30 +33,184 @@ namespace MapGenerator {
         }
 
         private int[,] GenerateMap() {
-            var map = CreateBlankMap();
+            var map = new int[size, size];
 
-            for (int row = 0; row < size; row++) {
-                GenerateRow(map, row);
+            GenerateRows(map);
+            GenerateColumns(map);
+
+            if (minFreedom > 0) {
+                AdjustRowRegions(map);
+                AdjustColumnRegions(map);
             }
+
+            AvoidEmptyRowRegions(map);
+            AvoidEmptyColumnRegions(map);
 
             return map;
         }
 
-        private int[,] CreateBlankMap() {
-            var blankMap = new int[size, size];
+        private void GenerateRows(int[,] map) {
+            var randomCells = GenerateRandomCells();
+            int index = 0;
 
             for (int row = 0; row < size; row++) {
                 for (int col = 0; col < size; col++) {
-                    blankMap[row, col] = 0;
+                    map[row, col] = randomCells[index];
+                    index++;
+                }
+            }
+        }
+
+        private void GenerateColumns(int[,] map) {
+            var randomCells = GenerateRandomCells();
+            int index = 0;
+
+            for (int col = 0; col < size; col++) {
+                for (int row = 0; row < size; row++) {
+                    map[row, col] = randomCells[index];
+                    index++;
+                }
+            }
+        }
+
+        private int[] GenerateRandomCells() {
+            int[] cells = new int[size * size];
+            int total = cells.Length;
+            int index = 0;
+
+            while (index < total) {
+                var cs = random.Next(0, 99) > threshold ? 1 : 0;
+                var length = random.Next(1, 3);
+
+                while (length > 0 && index < total) {
+                    cells[index] = cs;
+                    index++;
+                    length--;
                 }
             }
 
-            return blankMap;
+            return cells;
         }
 
-        private void GenerateRow(int[,] map, int row) {
+        private void AdjustRowRegions(int[,] map) {
+            for (int i = 0; i < size; i++) {
+                int[] cells = GetRowCells(map, i);
+
+                int df = FreedomFromArray(cells);
+
+                if (df == size) {
+                    /* Do not want completely empty region */
+                    InsertFilled(cells);
+                    continue;
+                }
+
+                /* Do not want to produce totally empty regions */
+                int min = Math.Min(size - 1, minFreedom + (i < 2 || i > size - 3 ? edgeBias : 0));
+
+                if (df >= min) {
+                    continue;
+                }
+
+                InsertEmpty(min - df, cells);
+
+                ResetRow(map, i, cells);
+            }
+        }
+
+        private void AdjustColumnRegions(int[,] map) {
+            for (int i = 0; i < size; i++) {
+                int[] cells = GetColumnCells(map, i);
+
+                int df = FreedomFromArray(cells);
+
+                if (df == size) {
+                    /* Do not want completely empty region */
+                    InsertFilled(cells);
+                    continue;
+                }
+
+                /* Do not want to produce totally empty regions */
+                int min = Math.Min(size - 1, minFreedom + (i < 2 || i > size - 3 ? edgeBias : 0));
+
+                if (df >= min) {
+                    continue;
+                }
+
+                InsertEmpty(min - df, cells);
+
+                ResetColumn(map, i, cells);
+            }
+        }
+
+        private int[] GetRowCells(int[,] map, int rowIdx) {
+            int[] row = new int[size];
+
             for (int col = 0; col < size; col++) {
-                map[row, col] = random.Next(1, 100) > 30 ? 1 : 0;
+                row[col] = map[rowIdx, col];
+            }
+
+            return row;
+        }
+
+        private void ResetRow(int[,] map, int rowIdx, int[] cells) {
+            for (int col = 0; col < size; col++) {
+                map[rowIdx, col] = cells[col];
+            }
+        }
+
+        private int[] GetColumnCells(int[,] map, int colIdx) {
+            int[] col = new int[size];
+
+            for (int row = 0; row < size; row++) {
+                col[row] = map[row, colIdx];
+            }
+
+            return col;
+        }
+
+        private void ResetColumn(int[,] map, int colIdx, int[] cells) {
+            for (int row = 0; row < size; row++) {
+                map[row, colIdx] = cells[row];
+            }
+        }
+
+        private void AvoidEmptyRowRegions(int[,] map) {
+            for (int i = 0; i < size; i++) {
+                int[] cells = GetRowCells(map, i);
+
+                int df = FreedomFromArray(cells);
+                if (df >= size) {
+                    InsertFilled(cells);
+                    ResetRow(map, i, cells);
+                    continue;
+                }
+            }
+        }
+
+        private void AvoidEmptyColumnRegions(int[,] map) {
+        }
+
+        private int GetWeight() {
+            switch (difficulty) {
+                case 1: return 58;
+                case 2: return 63;
+                case 3: return 67;
+                default: return 0;
+            }
+        }
+
+        private int GetEdgeBias() {
+            switch (difficulty) {
+                case 3: return 1;
+                default: return 0;
+            }
+        }
+
+        private int GetMinFreedom() {
+            switch (difficulty) {
+                case 2:
+                case 3: return 2;
+                default: return 1;
             }
         }
 
@@ -63,6 +226,56 @@ namespace MapGenerator {
             }
 
             Console.WriteLine(sb.ToString());
+        }
+
+        public void InsertEmpty(int replace, int[] cells) {
+            int count = 0;
+            int lim = cells.Length - 2;
+
+            for (int i = 1; i <= lim / 2 + 1; i++) {
+                int idx = i;
+
+                if (cells[idx] == 1) {
+                    cells[idx] = 0;
+                    if (++count == replace) {
+                        break;
+                    }
+                }
+
+                idx = lim - idx;
+
+                if (cells[idx] == 1) {
+                    cells[idx] = 0;
+                    if (++count == replace) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void InsertFilled(int[] cells) {
+            int lim = cells.Length - 2;
+
+            var ptr = random.Next(1, lim);
+            cells[ptr] = 1;
+        }
+
+        private int FreedomFromArray(int[] cells) {
+            var filled = 0; // count of filled cells
+            var blocks = 0; // count of filled groups
+            int length = cells.Length;
+
+            for (int i = 0; i < length; i++) {
+                if (cells[i] == 1) {
+                    filled++;
+
+                    if (i == 0 || cells[i - 1] == 0) {
+                        blocks++;
+                    }
+                }
+            }
+
+            return (length - filled - (blocks - 1));
         }
     }
 }
--- MapGenerator/Program.cs
+++ MapGenerator/Program.cs
@@ -8,10 +8,13 @@ namespace MapGenerator {
             Console.WriteLine("What size of puzzle would you like to generate? (8 - 15)");
             int size = GetNumber(8, 15);
 
+            Console.WriteLine("How difficult should these puzzles be? (1 - 3)");
+            int difficulty = GetNumber(1, 3);
+
             Console.WriteLine("How many puzzles should be generated?");
             int numberToCreate = GetNumber(1, 100);
 
-            Generator generator = new Generator(size);
+            Generator generator = new Generator(size, difficulty);
             generator.Run(numberToCreate);
 
             Console.WriteLine("Press any key to quit...");
