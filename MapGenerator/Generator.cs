bda75cb8306168020d50e0a458f7dd05f460882d
Sam Orme <ormesam@hotmail.com>
2020-06-19 01:12:05 +0100

    Add basic map generator project

---
+++ MapGenerator/Generator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Text;
+
+namespace MapGenerator {
+    public class Generator {
+        private readonly int size;
+        private readonly Random random;
+
+        public Generator(int size) {
+            this.size = size;
+            random = new Random();
+        }
+
+        public void Run(int numberToCreate) {
+            Console.WriteLine($"Generating {numberToCreate} maps at {size}x{size}");
+
+            for (int i = 0; i < numberToCreate; i++) {
+                int[,] map = GenerateMap();
+
+                DisplayMap(map);
+            }
+
+            Console.WriteLine($"Finished generating {numberToCreate} maps");
+        }
+
+        private int[,] GenerateMap() {
+            var map = CreateBlankMap();
+
+            for (int row = 0; row < size; row++) {
+                GenerateRow(map, row);
+            }
+
+            return map;
+        }
+
+        private int[,] CreateBlankMap() {
+            var blankMap = new int[size, size];
+
+            for (int row = 0; row < size; row++) {
+                for (int col = 0; col < size; col++) {
+                    blankMap[row, col] = 0;
+                }
+            }
+
+            return blankMap;
+        }
+
+        private void GenerateRow(int[,] map, int row) {
+            for (int col = 0; col < size; col++) {
+                map[row, col] = random.Next(1, 100) > 30 ? 1 : 0;
+            }
+        }
+
+        private void DisplayMap(int[,] map) {
+            StringBuilder sb = new StringBuilder();
+
+            for (int row = 0; row < size; row++) {
+                for (int col = 0; col < size; col++) {
+                    sb.Append(map[row, col] + " ");
+                }
+
+                sb.AppendLine();
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
---
+++ MapGenerator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MapGenerator {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello! Welcome to the Pixel Puzzle Generator");
+
+            Console.WriteLine("What size of puzzle would you like to generate? (8 - 15)");
+            int size = GetNumber(8, 15);
+
+            Console.WriteLine("How many puzzles should be generated?");
+            int numberToCreate = GetNumber(1, 100);
+
+            Generator generator = new Generator(size);
+            generator.Run(numberToCreate);
+
+            Console.WriteLine("Press any key to quit...");
+            Console.ReadLine();
+        }
+
+        private static int GetNumber(int lowerBound, int upperBound) {
+            int number = -1;
+
+            while (number < lowerBound || number > upperBound) {
+                int.TryParse(Console.ReadLine(), out number);
+            }
+
+            return number;
+        }
+    }
+}
