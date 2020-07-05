183560a4f72d9d4195b2db9442b90382c603350a
Sam Orme <ormesam@hotmail.com>
2020-07-05 21:29:45 +0100

    Increment version Fix constants

--- PixelPuzzle/PixelPuzzle/Utility/Constants.cs
+++ PixelPuzzle/PixelPuzzle/Utility/Constants.cs
@@ -7,7 +7,7 @@
         //public static string HintAdMobKey => "ca-app-pub-3940256099942544/5224354917";
         public static string BannerAdMobKey => "ca-app-pub-1992270298763477/8708583393";
         public static string HintAdMobKey => "ca-app-pub-1992270298763477/9358232347";
-        public static string AppCentreKey => "android=9c6930a2-f8b1-4993-8f0a-3d4631c184dc;";
+        public static string AppCentreKey => "android=1a4d8847-20f0-45c7-95af-51d93965d116;";
 #else
         public static string BannerAdMobKey => "ca-app-pub-1992270298763477/8708583393";
         public static string HintAdMobKey => "ca-app-pub-1992270298763477/9358232347";
