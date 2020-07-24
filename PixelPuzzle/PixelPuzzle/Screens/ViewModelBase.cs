b059eccbc4f442b756c880f8f4246d3adb586a29
Sam Orme <ormesam@hotmail.com>
2020-07-24 19:18:36 +0100

    Add some more info Remove Tests

--- PixelPuzzle/PixelPuzzle/Screens/ViewModelBase.cs
+++ PixelPuzzle/PixelPuzzle/Screens/ViewModelBase.cs
@@ -20,8 +20,15 @@ namespace PixelPuzzle.Screens {
 
         public double ScreenWidth => DeviceDisplay.MainDisplayInfo.Width;
 
-        // Detect if this is a small phone which doesn;t have much height compared to the width
-        // Might need to adjust the layout if this is true
+        /*
+         * Detect if this is a small phone which doesn;t have much height compared to the width
+         * Might need to adjust the layout if this is true
+         * Inch  = Ratio
+         * 3.4 = 1.8
+         * 3.3 = 1.66666
+         * 3.2 = 1.5
+         * 2.7 = 2.2
+        */
         public bool IsSmallScreen => ScreenHeight < 500 && (ScreenHeight / ScreenWidth) < 1.7;
     }
 }
