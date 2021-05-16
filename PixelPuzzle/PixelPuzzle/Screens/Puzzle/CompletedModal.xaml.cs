963cbf80814529f89c4c7dbedc42737acd1a678f
Sam Orme <ormesam@hotmail.com>
2021-05-16 14:12:08 +0100

    Fix issue of double tapping completed dialog

--- PixelPuzzle/PixelPuzzle/Screens/Puzzle/CompletedModal.xaml.cs
+++ PixelPuzzle/PixelPuzzle/Screens/Puzzle/CompletedModal.xaml.cs
@@ -6,12 +6,20 @@ using Xamarin.Forms.Xaml;
 namespace PixelPuzzle.Screens.Puzzle {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CompletedModal : ContentPage {
+        bool closing = false;
+
         public CompletedModal(MainContext context) {
             InitializeComponent();
             BindingContext = new TutorialCompleteModalViewModel(context);
         }
 
         private async void Continue_Clicked(object sender, EventArgs e) {
+            if (closing) {
+                return;
+            }
+
+            closing = true;
+
             await Navigation.PopModalAsync();
         }
     }
